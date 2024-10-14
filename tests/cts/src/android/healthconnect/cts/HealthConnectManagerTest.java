1eb46de3dc2c3907e039e70303f2c2125b958e21
Sophia Botz <sophiabotz@google.com>
2024-10-14 19:29:28 +0000

    Move CreateMedicalDataSource tests to new location
    Bug: 373385649
    Test: atest
    Flag: TEST_ONLY
    Change-Id: I172d540ba03b3eeb5e1fee60c8daa0933537b438

--- tests/cts/src/android/healthconnect/cts/HealthConnectManagerTest.java
+++ tests/cts/src/android/healthconnect/cts/HealthConnectManagerTest.java
@@ -63,7 +63,6 @@ import static android.healthconnect.cts.utils.PhrDataFactory.FHIR_RESOURCE_ID_IM
 import static android.healthconnect.cts.utils.PhrDataFactory.FHIR_VERSION_R4;
 import static android.healthconnect.cts.utils.PhrDataFactory.FHIR_VERSION_R4B;
 import static android.healthconnect.cts.utils.PhrDataFactory.FHIR_VERSION_UNSUPPORTED;
-import static android.healthconnect.cts.utils.PhrDataFactory.MAX_ALLOWED_MEDICAL_DATA_SOURCES;
 import static android.healthconnect.cts.utils.PhrDataFactory.getCreateMedicalDataSourceRequest;
 import static android.healthconnect.cts.utils.PhrDataFactory.getMedicalResourceId;
 import static android.healthconnect.cts.utils.PhrDataFactory.getUpsertMedicalResourceRequest;
@@ -1756,22 +1755,6 @@ public class HealthConnectManagerTest {
 
     @Test
     @RequiresFlagsEnabled({FLAG_PERSONAL_HEALTH_RECORD, FLAG_PERSONAL_HEALTH_RECORD_DATABASE})
-    public void testCreateMedicalDataSource_migrationInProgress_apiBlocked()
-            throws InterruptedException {
-        startMigrationWithShellPermissionIdentity();
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        HealthConnectReceiver<MedicalDataSource> receiver = new HealthConnectReceiver<>();
-
-        mManager.createMedicalDataSource(getCreateMedicalDataSourceRequest(), executor, receiver);
-
-        assertThat(receiver.assertAndGetException().getErrorCode())
-                .isEqualTo(HealthConnectException.ERROR_DATA_SYNC_IN_PROGRESS);
-
-        finishMigrationWithShellPermissionIdentity();
-    }
-
-    @Test
-    @RequiresFlagsEnabled({FLAG_PERSONAL_HEALTH_RECORD, FLAG_PERSONAL_HEALTH_RECORD_DATABASE})
     public void testReadMedicalResourcesByRequest_migrationInProgress_apiBlocked()
             throws InterruptedException {
         startMigrationWithShellPermissionIdentity();
@@ -1989,75 +1972,6 @@ public class HealthConnectManagerTest {
         }
 
         verifyRecordTypeResponse(response, expectedResponseMap);
-    }
-
-    @Test
-    @RequiresFlagsEnabled({FLAG_PERSONAL_HEALTH_RECORD, FLAG_PERSONAL_HEALTH_RECORD_DATABASE})
-    public void testCreateMedicalDataSource_succeeds() throws InterruptedException {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        HealthConnectReceiver<MedicalDataSource> receiver = new HealthConnectReceiver<>();
-        CreateMedicalDataSourceRequest request = getCreateMedicalDataSourceRequest();
-
-        mManager.createMedicalDataSource(request, executor, receiver);
-
-        MedicalDataSource responseDataSource = receiver.getResponse();
-        assertThat(responseDataSource).isInstanceOf(MedicalDataSource.class);
-        assertThat(responseDataSource.getId()).isNotEmpty();
-        assertThat(responseDataSource.getFhirBaseUri()).isEqualTo(request.getFhirBaseUri());
-        assertThat(responseDataSource.getDisplayName()).isEqualTo(request.getDisplayName());
-        assertThat(responseDataSource.getFhirVersion()).isEqualTo(request.getFhirVersion());
-        assertThat(responseDataSource.getPackageName()).isEqualTo(APP_PACKAGE_NAME);
-    }
-
-    @Test
-    @RequiresFlagsEnabled({FLAG_PERSONAL_HEALTH_RECORD, FLAG_PERSONAL_HEALTH_RECORD_DATABASE})
-    public void testCreateMedicalDataSource_hasDataManagementPermission_throws()
-            throws InterruptedException {
-        HealthConnectReceiver<MedicalDataSource> receiver = new HealthConnectReceiver<>();
-
-        SystemUtil.runWithShellPermissionIdentity(
-                () ->
-                        mManager.createMedicalDataSource(
-                                getCreateMedicalDataSourceRequest(),
-                                Executors.newSingleThreadExecutor(),
-                                receiver),
-                MANAGE_HEALTH_DATA);
-
-        assertThat(receiver.assertAndGetException().getErrorCode())
-                .isEqualTo(HealthConnectException.ERROR_SECURITY);
-    }
-
-    @Test
-    @RequiresFlagsEnabled({FLAG_PERSONAL_HEALTH_RECORD, FLAG_PERSONAL_HEALTH_RECORD_DATABASE})
-    public void testCreateMedicalDataSource_maxNumberOfSources_succeeds()
-            throws InterruptedException {
-        for (int i = 0; i < MAX_ALLOWED_MEDICAL_DATA_SOURCES - 1; i++) {
-            String suffix = String.valueOf(i);
-            createDataSource(getCreateMedicalDataSourceRequest(suffix));
-        }
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        HealthConnectReceiver<MedicalDataSource> receiver = new HealthConnectReceiver<>();
-        mManager.createMedicalDataSource(getCreateMedicalDataSourceRequest(), executor, receiver);
-
-        receiver.verifyNoExceptionOrThrow();
-    }
-
-    @Test
-    @RequiresFlagsEnabled({FLAG_PERSONAL_HEALTH_RECORD, FLAG_PERSONAL_HEALTH_RECORD_DATABASE})
-    public void testCreateMedicalDataSource_moreThanAllowedMax_throws()
-            throws InterruptedException {
-        for (int i = 0; i < MAX_ALLOWED_MEDICAL_DATA_SOURCES; i++) {
-            String suffix = String.valueOf(i);
-            createDataSource(getCreateMedicalDataSourceRequest(suffix));
-        }
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        HealthConnectReceiver<MedicalDataSource> receiver = new HealthConnectReceiver<>();
-        mManager.createMedicalDataSource(getCreateMedicalDataSourceRequest(), executor, receiver);
-
-        assertThat(receiver.assertAndGetException().getErrorCode())
-                .isEqualTo(HealthConnectException.ERROR_INVALID_ARGUMENT);
     }
 
     @Test
