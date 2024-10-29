1a6bc2eb0763e47b9e730ba04a8e38fe86acc140
Tallie Blanshard <tallie@google.com>
2024-10-29 10:57:00 +0000

    Reapply "Extend CloudBackupManager to parse user settings as byte array."
    This reverts commit 9d860e262a42720674f968db9357d344523bdb9b.
    
    Fixes build problem with the name change for 'DatabaseContext' to 'StorageContext'.
    
    Bug: b/369801652
    Flag: com.android.healthfitness.flags.cloud_backup_and_restore
    Test: atest
    Change-Id: Ia43c648533c8e0ff648107c58343d50e9100b412

--- tests/unittests/src/com/android/server/healthconnect/backuprestore/CloudBackupManagerTest.java
+++ tests/unittests/src/com/android/server/healthconnect/backuprestore/CloudBackupManagerTest.java
@@ -35,6 +35,7 @@ import com.android.server.healthconnect.injector.HealthConnectInjector;
 import com.android.server.healthconnect.injector.HealthConnectInjectorImpl;
 import com.android.server.healthconnect.permission.FirstGrantTimeManager;
 import com.android.server.healthconnect.permission.HealthPermissionIntentAppsTracker;
+import com.android.server.healthconnect.storage.ExportImportSettingsStorage;
 import com.android.server.healthconnect.storage.TransactionManager;
 import com.android.server.healthconnect.storage.datatypehelpers.AccessLogsHelper;
 import com.android.server.healthconnect.storage.datatypehelpers.AppInfoHelper;
@@ -42,6 +43,8 @@ import com.android.server.healthconnect.storage.datatypehelpers.ChangeLogsHelper
 import com.android.server.healthconnect.storage.datatypehelpers.ChangeLogsRequestHelper;
 import com.android.server.healthconnect.storage.datatypehelpers.DeviceInfoHelper;
 import com.android.server.healthconnect.storage.datatypehelpers.HealthConnectDatabaseTestRule;
+import com.android.server.healthconnect.storage.datatypehelpers.HealthDataCategoryPriorityHelper;
+import com.android.server.healthconnect.storage.datatypehelpers.PreferenceHelper;
 import com.android.server.healthconnect.storage.datatypehelpers.TransactionTestUtils;
 import com.android.server.healthconnect.storage.utils.InternalHealthConnectMappings;
 
@@ -102,6 +105,9 @@ public class CloudBackupManagerTest {
         AppInfoHelper appInfoHelper = healthConnectInjector.getAppInfoHelper();
         AccessLogsHelper accessLogsHelper = healthConnectInjector.getAccessLogsHelper();
         DeviceInfoHelper deviceInfoHelper = healthConnectInjector.getDeviceInfoHelper();
+        HealthDataCategoryPriorityHelper priorityHelper =
+                healthConnectInjector.getHealthDataCategoryPriorityHelper();
+        PreferenceHelper preferenceHelper = healthConnectInjector.getPreferenceHelper();
         HealthConnectMappings healthConnectMappings =
                 healthConnectInjector.getHealthConnectMappings();
         InternalHealthConnectMappings internalHealthConnectMappings =
@@ -109,6 +115,8 @@ public class CloudBackupManagerTest {
         ChangeLogsHelper changeLogsHelper = healthConnectInjector.getChangeLogsHelper();
         ChangeLogsRequestHelper changeLogsRequestHelper =
                 healthConnectInjector.getChangeLogsRequestHelper();
+        ExportImportSettingsStorage exportImportSettingsStorage =
+                healthConnectInjector.getExportImportSettingsStorage();
 
         mCloudBackupManager =
                 new CloudBackupManager(
@@ -119,7 +127,10 @@ public class CloudBackupManagerTest {
                         healthConnectMappings,
                         internalHealthConnectMappings,
                         changeLogsHelper,
-                        changeLogsRequestHelper);
+                        changeLogsRequestHelper,
+                        priorityHelper,
+                        preferenceHelper,
+                        exportImportSettingsStorage);
     }
 
     @After
