560bcda275f18b835d8a2932bc71124245a5a626
Magdi <magdi@google.com>
2025-01-28 10:41:45 +0000

    Move disabling animation to device setup
    Bug: 378699366
    Bug: 384724815
    Test: atest --iterations=10 HealthConnectControllerUITests
    Flag: NONE test fix
    Change-Id: I222d55e8db8c2aeeae8a1c4f17625f50d074b042

--- apk/tests/src/com/android/healthconnect/controller/tests/deletion/DeletionFragmentTest.kt
+++ apk/tests/src/com/android/healthconnect/controller/tests/deletion/DeletionFragmentTest.kt
@@ -43,7 +43,6 @@ import com.android.healthconnect.controller.permissions.data.FitnessPermissionTy
 import com.android.healthconnect.controller.tests.utils.TEST_APP_NAME
 import com.android.healthconnect.controller.tests.utils.TEST_APP_PACKAGE_NAME
 import com.android.healthconnect.controller.tests.utils.launchFragment
-import com.android.healthconnect.controller.tests.utils.toggleAnimation
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -66,12 +65,6 @@ class DeletionFragmentTest {
     fun setup() {
         hiltRule.inject()
         whenever(viewModel.isInactiveApp).then { false }
-        toggleAnimation(false)
-    }
-
-    @After
-    fun tearDown() {
-        toggleAnimation(true)
     }
 
     // Delete all data flow
